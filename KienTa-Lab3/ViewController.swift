47e2a1e896c1e66dc8061d9475a68e15eedc9dd0
Kien Ta <kienta@Kiens-MacBook-Pro.local>
2020-10-23 18:32:22 +0700

    project created

---
+++ KienTa-Lab3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KienTa-Lab3
+//
+//  Created by Kien Ta on 10/23/20.
+//  Copyright © 2020 Kien Ta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
